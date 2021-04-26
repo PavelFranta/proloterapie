702daf724d579cbf7f28ae5d092ed6d46d3a0044
Pavel Franta <pavel.franta@zonky.cz>
2021-04-26 20:07:37 +0200

    nav enhanced

--- src/lib/nav.svelte
+++ src/lib/nav.svelte
@@ -1,44 +1,10 @@
 <script>
-	 import Menu32 from "carbon-icons-svelte/lib/Menu32";
-
-	// Show mobile icon and display menu
-	let showMobileMenu = false;
   let y;
-
-	const navItemsRight = [
-    { label: 'PROLOTERAPIE', href: '/' },
-		{ label: 'kontakt', href: '/contact' }
-	];
-
-	// Mobile menu click event handler
-	const handleMobileIconClick = () => (showMobileMenu = !showMobileMenu);
-
-	const handleClick = () => {
-    if (window.innerWidth < 480) {
-      showMobileMenu = false;
-    }
-  }
 </script>
 
 <svelte:window bind:scrollY={y}/>
-<nav class="fixed w-full z-10 bg-white text-black md:text-white" class:scrollYBiggerThanZero="{y}" class:scrollZero="{y === 0}">
-	<div class="inner font-mono w-full">
-    <div class="w-full flex justify-between">
-      <a href="/" class="prolo-hidden text-2xl">PROLOTERAPIE</a>
-      <div class="prolo-hidden">
-        <Menu32 on:click={handleMobileIconClick} class="h-16 w-16 -mt-3"/>
-      </div>
-    </div>
-		<div class="w-full">
-			<ul class={`flex justify-between navbar-list${showMobileMenu ? ' mobile' : ''}`}>
-				{#each navItemsRight as item}
-					<li>
-						<a on:click={handleClick} href={item.href} class={showMobileMenu ? "text-white" : ""}>{item.label}</a>
-					</li>
-				{/each}
-			</ul>
-		</div>
-	</div>
+<nav class="fixed w-full z-10 flex items-center bg-white text-black md:text-white" class:scrollYBiggerThanZero="{y}" class:scrollZero="{y === 0}">
+	<a href="/" class="text-2xl pl-4">PROLOTERAPIE</a>
 </nav>
 <style>
 	nav {
@@ -56,10 +22,6 @@
     @apply text-black
   }
 
-  .scrollBiggerThenZeroHamburgerMenu {
-    background-color: black;
-  }
-
   .scrollZero {
     transition-property: all;
     transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
@@ -69,65 +31,4 @@
     @apply text-white
   }
 
-	.inner {
-		display: block;
-		padding: 20px;
-		box-sizing: border-box;
-		height: 5rem;
-	}
-
-	.navbar-list {
-		display: none;
-		margin: 0;
-		padding: 0 40px;
-	}
-
-	.navbar-list.mobile {
-    padding-top: 1rem;
-		background-color:black;
-		position: fixed;
-		display: block;
-		height: 100%;
-    width: 100%;
-		bottom: 0;
-		left: 0;
-	}
-
-	.navbar-list li {
-		list-style-type: none;
-		position: relative;
-	}
-
-	.navbar-list li:before {
-		content: '';
-		position: absolute;
-		bottom: 0;
-		left: 0;
-		height: 1px;
-		background-color: black;
-	}
-
-	.navbar-list a {
-		text-decoration: none;
-		display: flex;
-		height: 45px;
-		align-items: center;
-		padding: 0 10px;
-		font-size: 24px;
-	}
-
-	@media only screen and (min-width: 480px) {
-		.navbar-list {
-			display: flex;
-			padding: 0;
-		}
-
-		.navbar-list a {
-			display: inline-flex;
-		}
-
-    .prolo-hidden {
-      display: none;
-    }
-  }
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,78 +6,89 @@
 </script>
 
 <div class="prague">
-	<img src={pragueSrc} alt="skull" class="w-full xl:big-resolution"/>
+	<img src={pragueSrc} alt="skull" class="w-full xl:big-resolution" />
 </div>
 <h2 class="px-6 py-4 leading-10 text-2xl text-center bg-white">
 	Proloterapie je léčebná metoda založená na stimulaci přirozeného procesu hojení těla.
 </h2>
 <div class="triangle-down" />
 <div class="max-w-screen-lg m-auto">
-		<div class="bg-black md:h-96 flex flex-col md:flex-row justify-between py-8 text-center text-white">
-			<div class="flex-1 p-6">
-				<ReminderMedical32 class="text-green-900 m-auto mb-6 h-16 w-16" />
-				<h4 class="mb-4 font-bold text-3xl">Jak se to provádí</h4>
-				<p class="flex-1">
-					Tato stimulace se provádí sérií injekcí, jejíž hlavní složkou je roztok dextrózy, cukru.
-				</p>
-			</div>
-			<div class="flex-1 p-6">
-				<Sunny32 class="text-green-900 m-auto mb-6 h-16 w-16" />
-				<h4 class="mb-4 font-bold text-3xl">Výsledky a pohodlí</h4>
-				<p class="flex-1">
-					Hlavní výhodou této metody je vysoká úspěšnost( 80%), minimální vedlejší účinky a
-					ambulantní styl léčby, většinou není nutná ani pracovní neschopnost.
-				</p>
-			</div>
-			<div class="flex-1 p-6">
-				<Network_432 class="text-green-900 m-auto mb-6 h-16 w-16" />
-				<h4 class="mb-4 font-bold text-3xl">Historie</h4>
-				<p class="flex-1">
-					Proloterapie se úspěšně používá již více než 50 let u léčby poškozených vazů, šlach,
-					kloubů a meziobratlových plotének hlavně ve Spojených státech a západní Evropě.
-				</p>
-			</div>
+	<div
+		class="bg-black md:h-96 flex flex-col md:flex-row justify-between py-8 text-center text-white"
+	>
+		<div class="flex-1 p-6">
+			<ReminderMedical32 class="text-green-900 m-auto mb-6 h-16 w-16" />
+			<h4 class="mb-4 font-bold text-3xl">S čím mi pomůže?</h4>
+			<p class="flex-1">
+				Proloterapie se ve Spojených státech a v západní Evropě již více než 50 let úspěšně používá
+				při léčbě poškozených vazů, šlach, kloubů a meziobratlových plotének.
+			</p>
+		</div>
+		<div class="flex-1 p-6">
+			<Sunny32 class="text-green-900 m-auto mb-6 h-16 w-16" />
+			<h4 class="mb-4 font-bold text-3xl">Výhody této metody</h4>
+			<p class="flex-1">
+				Hlavní výhodou proloterapie je její vysoká úspěšnost (80%), minimální vedlejší účinky a
+				ambulantní styl léčby. Většinou není nutná ani pracovní neschopnost.
+			</p>
+		</div>
+		<div class="flex-1 p-6">
+			<Network_432 class="text-green-900 m-auto mb-6 h-16 w-16" />
+			<h4 class="mb-4 font-bold text-3xl">Jak se to provádí?</h4>
+			<p class="flex-1">
+				Proloterapeutická stimulace se provádí sérií injekcí, jejichž hlavní složkou je cukerný
+				roztok z dextrózy.
+			</p>
 		</div>
 	</div>
+</div>
 <h2 class="px-6 py-4 leading-10 text-2xl text-center bg-white">Detailní popis léčby</h2>
 <div class="triangle-down" />
 <div class="bg-green-900">
 	<div class="max-w-screen-lg m-auto text-white treatment-detail py-10 md:py-20 bg-green-900">
 		<p>
-			Pacientovi je na léčené místo injekčně vpraven roztok dextrózy, který vytvoří lokální zánět,
-			čímž mobilizuje síly imunitního systému aby zasáhl a začal toto místo hromadně regenerovat
+			Pacientovi je na léčené místo injekčně vpraven roztok dextrózy, který vytvoří lokální zánět.
 		</p>
-		<p>Injekční aplikace trvá několik málo vteřin a je velmi dobře snášena i méně statečnými :)</p>
 		<p>
-			Pro hluboké proléčení je třeba aplikaci několikrát opakovat v týdenních intervalech (maximálně
-			však 10 týdnů)
+			Tím dojde k mobilizací imunitního systému, který zasáhne a začne toto místo intenzivně
+			regenerovat.
 		</p>
-		<p>Účinek léčby proloterapií je následně trvalý a není třeba jej obnovovat.</p>
+		<p>Injekční aplikace trvá několik málo vteřin a je velmi dobře snášena i méně statečnými.</p>
+		<p>
+			Pro hluboké proléčení je třeba aplikaci v týdenních intervalech několikrát opakovat. Maximálně
+			však 10 týdnů.
+		</p>
+		<p>Účinek léčby proloterapií je trvalý a není třeba jej obnovovat.</p>
 	</div>
 </div>
+<h2 class="px-6 py-4 leading-10 text-2xl text-center bg-white">
+	Domluvte si termín své konzultace
+</h2>
+<div class="triangle-down" />
+
 <style>
 	.prague {
 		max-height: 500px;
 		overflow: hidden;
 	}
-	
+
 	@media only screen and (min-width: 1800px) {
 		.prague img {
 			position: relative;
 			top: -250px;
 		}
 	}
-	
+
 	@media only screen and (min-width: 2730px) {
 		.prague img {
-		position: relative;
-		top: -400px;
-	}
+			position: relative;
+			top: -400px;
+		}
 	}
 
 	.prague img {
 		max-height: 100%;
-  	max-width: 100%;
+		max-width: 100%;
 	}
 
 	.triangle-down {
